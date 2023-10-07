8bee47e9bd79e608cf6a3caa3f7d207bdac345c7
yosuaelw <cuatiawww@gmail.com>
2023-10-07 08:27:01 +0700

    iteration 2 : done

--- js/index.js
+++ js/index.js
@@ -1,11 +1,16 @@
 // Iteration 1: Names and Input
 // Hacker 1
-let hacker1 = "driver";
+let hacker1 = "Yosua";
 console.log("The driver's name is " + hacker1);
 // Hacker 2
-let hacker2 = "navigator";
-console.log("The driver's name is " + hacker2);
+let hacker2 = "yosua";
+console.log(`The driver's name is ${hacker2}`); // i use backtick(``)
 // Iteration 2: Conditionals
-
-
+if (hacker1.length > hacker2.length){
+    console.log(`The driver has the longest name, it has ${hacker1.length} characters.`)
+} else if (hacker1.length < hacker2.length) {
+    console.log(`It seems that the navigator has the longest name, it has ${hacker2.length} characters.`)
+} else {
+    console.log(`Wow, you both have equally long names, ${hacker1.length} characters!`)
+}
 // Iteration 3: Loops
