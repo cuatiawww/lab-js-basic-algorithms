e8138c61fa39b81535d294d81dd6b287fd3b5ca0
yosuaelw <cuatiawww@gmail.com>
2023-10-07 08:11:41 +0700

    iteration 1 - Done

--- js/index.js
+++ js/index.js
@@ -1,5 +1,10 @@
 // Iteration 1: Names and Input
-//
+// Hacker 1
+let hacker1 = "driver";
+console.log("The driver's name is " + hacker1);
+// Hacker 2
+let hacker2 = "navigator";
+console.log("The driver's name is " + hacker2);
 // Iteration 2: Conditionals
 
 
